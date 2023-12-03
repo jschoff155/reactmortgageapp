fe5434dde5e5d2fce8fb2a8d063f2baaa0032568
Jacob Schoff <jschoff155@gmail.com>
2023-12-02 22:45:19 -0600

    Completed mortgage payment application

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
             <TenureSelection mpdata={mpdata} setmpdata={setmpdata}/>
           </Grid>
           <Grid item xs= {12} md={6}>
-            <Result />
+            <Result mpdata={mpdata}/>
           </Grid>
         </Grid>
       </Container>
--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,10 +1,52 @@
 import React from 'react'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
-const Result = () => {
+ChartJS.register(ArcElement, Tooltip, Legend);
+const Result = ({mpdata}) => {
+  
+  const{
+    homeValue,
+    loanAmount,
+    loanTerm,
+    interestRate,
+  } = mpdata
+
+  const totalLoanMonths = loanTerm * 12;
+  const interestPerMonth = interestRate /100 /12;
+  const monthlyPayment = 
+  (loanAmount * interestPerMonth *(1+ interestPerMonth) ** totalLoanMonths) /
+  ((1 + interestPerMonth) ** totalLoanMonths - 1);
+
+  const totalInterestGenerated = monthlyPayment * totalLoanMonths - loanAmount;
+
+  const pieChartData = {
+    labels: ['Principal', 'Interest'],
+    datasets: [
+      {
+        label: 'Amount Paid',
+        data: [homeValue, totalInterestGenerated.toFixed(2)],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  
+
   return (
     <div>
-      Result
+      Monthly Payment: $ {monthlyPayment.toFixed(2)}
+      <Pie data={pieChartData}/>
     </div>
+    
+    
   )
 }
 
